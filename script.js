5698f8ab5fa8aa61cda43b1847a9ebf93b66c213
Roman <kosmosxuesmos@gmail.com>
2021-08-05 15:55:45 +0300

    Second update script.js

--- script.js
+++ script.js
@@ -1,44 +1,76 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
+// мой гнусный вариант в условиях:((((( )))))
+    // if ((firstLastFilm && firstRateFilm) == null) {
+    //     i--;
+    // } else if ((firstLastFilm && firstRateFilm) == "" || firstLastFilm.length > 50) {
+    //     i--;
+    // } else {
+    //     personalMovieDB.movies[firstLastFilm] = firstRateFilm;
+    // }
 
 'use strict';
 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-let firstLastFilm = prompt("Один из последних просмотренных фильмов?", "");
-let firstRateFilm = prompt("На сколько оцените его?", "");
-let secondLastFilm = prompt("Один из последних просмотренных фильмов?", "");
-let secondRateFilm = prompt("На сколько оцените его?", "");
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        [firstLastFilm]: firstRateFilm,
-        [secondLastFilm]: secondRateFilm
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+for (let i = 0; i < 2; i++) {
+    let firstLastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    let firstRateFilm = prompt("На сколько оцените его?", "");
+
+    if (firstLastFilm != null && firstRateFilm != null && firstLastFilm != "" && 
+        firstRateFilm != "" && firstLastFilm.length < 50) {
+            personalMovieDB.movies[firstLastFilm] = firstRateFilm;
+    } else {
+        i--;
+    }
+}
+
+// Второй вариант цикла!!!
+// let i = 0;
+// while(i < 2) {
+//     let firstLastFilm = prompt("Один из последних просмотренных фильмов?", "");
+//     let firstRateFilm = prompt("На сколько оцените его?", "");
+
+//     if (firstLastFilm != null && firstRateFilm != null && firstLastFilm != "" && 
+//         firstRateFilm != "" && firstLastFilm.length < 50) {
+//             personalMovieDB.movies[firstLastFilm] = firstRateFilm;
+//     } else {
+//         i--;
+//     }
+//     i++;
+// }
+
+// Третий вариант цикла!!!
+// let i = 0;
+// do {
+//     let firstLastFilm = prompt("Один из последних просмотренных фильмов?", "");
+//     let firstRateFilm = prompt("На сколько оцените его?", "");
+
+//     if (firstLastFilm != null && firstRateFilm != null && firstLastFilm != "" && 
+//         firstRateFilm != "" && firstLastFilm.length < 50) {
+//             personalMovieDB.movies[firstLastFilm] = firstRateFilm;
+//     } else {
+//         i--;
+//     }
+//     i++;
+// } while (i < 2);
+
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB.movies);
 
